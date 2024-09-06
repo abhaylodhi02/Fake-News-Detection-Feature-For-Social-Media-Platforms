fa8d46f98e6031d37d5b250d13daeba223268bfd
abhaylodhi02 <abhaylodhi108@gmail.com>
2024-09-07 00:29:40 +0530

    filtered

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,11 @@
 import { NavLink } from "react-router-dom";
 
 export default function Home() {
-    const text = `Let's Get Started!`
+    //const text = `Let's Get Started!`
 
     return (
         <div className="mx-auto w-full max-w-7xl">
-            <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16" style={{height:"400px"}}>
+            <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16" >
                 
                 <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
@@ -30,11 +30,11 @@ export default function Home() {
                 </div>
             </aside>
 
-            <div className="grid  place-items-center sm:mt-20">
+            {/* <div className="grid  place-items-center sm:mt-20">
                 <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
-            </div>
+            </div> */}
 
-            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">{text}</h1>
+            {/* <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">{text}</h1> */}
         </div>
     );
 }
