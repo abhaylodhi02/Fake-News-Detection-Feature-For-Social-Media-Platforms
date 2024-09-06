235b9543c63b2144fae48c2880b9f823012fb2b1
abhaylodhi02 <abhaylodhi108@gmail.com>
2024-09-07 01:37:12 +0530

    vercel

--- server/app.py
+++ server/app.py
@@ -86,7 +86,4 @@ def preprocess_filter(text, stem=False):
             tokens.append(token)
     return " ".join(tokens)
 
-if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+
