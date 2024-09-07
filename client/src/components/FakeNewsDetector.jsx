d80766d2230e0b0258144b30c1393ec71a6f8ece
Abhay Lodhi <123784791+abhaylodhi02@users.noreply.github.com>
2024-09-07 14:24:01 +0530

    Update FakeNewsDetector.jsx

--- client/src/components/FakeNewsDetector.jsx
+++ client/src/components/FakeNewsDetector.jsx
@@ -80,8 +80,9 @@ const FakeNewsDetector = () => {
           Upload
         </button>
       </div>
-      <h3 className='message'><i>(Click on the icon to upload the image or visit the Github link mentioned below to understand the working.)</i><br>
-      <i>Note: Due to slow servers prediction may take some time.</i></h3>
+      <h3 className='message'><i>(Click on the icon to upload the image or visit the Github link mentioned below to understand the working.)</i></h3>
+      </br>
+      <h3 className ='message'><i>Note: Due to slow servers prediction may take some time.</i></h3>
     </div>
   );
 };
