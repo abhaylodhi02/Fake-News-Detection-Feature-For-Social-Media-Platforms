d0a698d99885b38c4b6bb491bf42cdbd5159c172
abhaylodhi02 <abhaylodhi108@gmail.com>
2024-09-07 14:30:03 +0530

    note message

--- client/src/components/FakeNewsDetector.jsx
+++ client/src/components/FakeNewsDetector.jsx
@@ -81,6 +81,7 @@ const FakeNewsDetector = () => {
         </button>
       </div>
       <h3 className='message'><i>(Click on the icon to upload the image or visit the Github link mentioned below to understand the working.)</i></h3>
+      <h3 className='message'><i>Note: Due to slow servers prediction may take some time.</i></h3>
     </div>
   );
 };
